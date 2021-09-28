075224aed9eccd95e58172d4e24fb024e121d3d5
André Almeida <andre.almeida@iteris.com.br>
2021-09-27 22:30:10 -0300

    refactor endpoint file

---
+++ Nerdflix/Resource/NetworkService/API.swift
@@ -0,0 +1,20 @@
+//
+//  Endpoint.swift
+//  Nerdflix
+//
+//  Created by André Felipe de Sousa Almeida - AAD on 25/09/21.
+//
+
+import Foundation
+
+
+struct API{
+    static private(set) var token = "k_tye9espx"
+    static private(set) var baseUrl = "https://imdb-api.com/pt/API"
+}
+
+//MARK: - TOKENS
+//k_itv91nl9
+//k_5zhyzkpo
+//k_tye9espx
+
--- Nerdflix/Resource/NetworkService/Endpoint.swift
+++ Nerdflix/Resource/NetworkService/Endpoint.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+enum Endpoint: String {
+    case Top250Movies = "Top 250 Filmes"
+    case MostPopularMovies = "Mais Populares"
+    case InTheaters = "Nos cinemas"
+    case ComingSoon = "Em Breve"
+    case MovieDetail
+    
+    
+    func ulrString(id: String? = nil) -> String {
+        switch self {
+        case .Top250Movies:
+            return API.baseUrl + "/Top250Movies/" + API.token
+        case .MostPopularMovies:
+            return API.baseUrl + "/MostPopularMovies/" + API.token
+        case .MovieDetail:
+            return API.baseUrl + "/Title/" + API.token + "/\(id ?? "")"
+        case .InTheaters:
+            return API.baseUrl + "/InTheaters/" + API.token
+        case .ComingSoon:
+            return API.baseUrl + "/ComingSoon/" + API.token
+        }
+    }
+}
